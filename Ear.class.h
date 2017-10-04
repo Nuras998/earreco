0d81df53521e51d01d6f9aa5ad15581ed62d25f2
Łukasz <nuras9988@gmail.com>
2017-10-04 16:12:49 +0200

    Ear class header file edited.

--- Ear.class.h
+++ Ear.class.h
@@ -2,25 +2,26 @@
 #include <vector>
 class Ear {
 
-
-
 public:
-	static cv::CascadeClassifier earClassifier;
-	Ear();
-	Ear(cv::Mat img);
-	Ear(const char* fileName);
-	cv::Mat getOriginal();
-	cv::Mat getSelectedEar();
-	cv::Mat getExtractedEar();
-	void load(const char* fileName);
-	cv::Rect findEar();
-	void execute();
-	void extractEar(cv::Rect ears); 
-	bool isReady;
+        Ear();
+        Ear(cv::Mat img);
+        Ear(const char* fileName);
+        cv::Mat getOriginal();
+        cv::Mat getSelectedEar();
+        cv::Mat getExtractedEar();
+        void load(const char* fileName);
+        bool isReady;
 private:
-	cv::Mat originalImg;
-	cv::Mat earSelected;
-	cv::Mat extractedEar;
-	cv::Rect earCords;
-	void findReference();
-}; 
+        cv::Rect findEar();
+        void findReference();
+        void execute();
+        void calcHaarFeature(cv::Mat img);
+        void extractEar(cv::Rect cords);
+
+        static cv::CascadeClassifier earClassifier;
+        cv::Mat originalImg;
+        cv::Mat earSelected;
+        cv::Mat extractedEar;
+        cv::Rect earCords;
+};
+
